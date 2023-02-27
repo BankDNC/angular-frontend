0de7df2fa9ce8d89825bfcc39b3bac0dddb52faf
caobandoc <obandocarlosandres@gmail.com>
2023-02-27 14:02:15 -0500

    top Bar

--- src/app/dashboard/dashboard.module.ts
+++ src/app/dashboard/dashboard.module.ts
@@ -2,16 +2,20 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { DashboardComponent } from './components/dashboard/dashboard.component';
 import { DashboardRoutingModule } from './dashboard-routing.module';
+import { ToolbarComponent } from './components/toolbar/toolbar.component';
+import { MaterialModule } from '../shared/material.module';
 
 
 
 @NgModule({
   declarations: [
-    DashboardComponent
+    DashboardComponent,
+    ToolbarComponent
   ],
   imports: [
     CommonModule,
-    DashboardRoutingModule
+    DashboardRoutingModule,
+    MaterialModule
   ]
 })
 export class DashboardModule { }
--- src/app/shared/services/auth.service.ts
+++ src/app/shared/services/auth.service.ts
@@ -5,7 +5,6 @@ import { TokenService } from './token.service';
 import { environment } from './../../../environments/environment';
 import { Login } from '../models/login.model';
 import { tap } from 'rxjs/operators';
-import { BehaviorSubject } from 'rxjs';
 import { UserLogin } from '../models/user.model';
 
 @Injectable({
@@ -14,9 +13,6 @@ import { UserLogin } from '../models/user.model';
 export class AuthService {
 
   private apiUrl = `${environment.API_URL}/auth`;
-  //private user = new BehaviorSubject<UserLogin | null>(null);
-
-  //user$ = this.user.asObservable();
 
   constructor(
     private http: HttpClient,
@@ -29,18 +25,10 @@ export class AuthService {
         tap({
           next: (response: any) => {
             this.tokenService.saveToken(response.token)
-            //this.decriptTokenToUser(response.token);
           },
         })
       );
   }
-
-  /*decriptTokenToUser(token: string) {
-    if (token) {
-      const user = JSON.parse(atob(token.split('.')[1])) as User;
-      this.user.next(user);
-    }
-  }*/
 
   register(user: UserLogin) {
     return this.http.post(`${this.apiUrl}/register`, user)
